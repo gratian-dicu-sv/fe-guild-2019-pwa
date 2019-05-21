88a0af1171e694d762a3a63e8ab87b48de594496
Gratian Dicu <gratian.dicu@softvision.com>
2019-05-21 13:23:41 +0300

    Updates

--- src/js/feed.js
+++ src/js/feed.js
@@ -7,25 +7,25 @@ const closeCreatePostModalButton = document.querySelector(
 const openCreatePostModal = () => {
   createPostArea.style.transform = "translateY(0)";
 
-  if (deferredPrompt) {
-    deferredPrompt.prompt();
+  // if (deferredPrompt) {
+  //   deferredPrompt.prompt();
 
-    // Determine the user's choice - returned as a Promise
-    deferredPrompt.userChoice.then(result => {
-      console.log(result.outcome);
+  //   // Determine the user's choice - returned as a Promise
+  //   deferredPrompt.userChoice.then(result => {
+  //     console.log(result.outcome);
 
-      // Based on the user's choice, decide how to proceed
-      if (result.outcome === "dismissed") {
-        // Send to analytics
-        console.log("User cancelled installation");
-      } else {
-        // Send to analytics
-        console.log("User added to home screen");
-      }
-    });
+  //     // Based on the user's choice, decide how to proceed
+  //     if (result.outcome === "dismissed") {
+  //       // Send to analytics
+  //       console.log("User cancelled installation");
+  //     } else {
+  //       // Send to analytics
+  //       console.log("User added to home screen");
+  //     }
+  //   });
 
-    deferredPrompt = null;
-  }
+  //   deferredPrompt = null;
+  // }
 };
 
 const closeCreatePostModal = () =>
